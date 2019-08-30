6e7fc08d2eacd51cf429d1c56c82b748eeb30b4c
Hiroka Daigo <daigo.hiroka@PC-1250.local>
2019-08-30 19:57:42 +0900

    Working confirmed for dogs-vs-cats dataset

--- tf/dataset.py
+++ tf/dataset.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import random
 from glob import glob
 from abc import abstractmethod, ABCMeta
 from functools import partial
@@ -9,6 +10,7 @@ class Base(metaclass=ABCMeta):
                  dataset_dir,
                  train_or_test,
                  batch_size=1,
+                 validation_split=0.1,
                  resize_shape=None,
                  crop_shape=None,
                  rotate=False,
@@ -19,6 +21,7 @@ class Base(metaclass=ABCMeta):
           - dataset_dir: string of /path/to/dataset-directory
           - train_or_test: train or test argument
           - batch_size: int for batch size
+          - validation_split: validation split ratio, should be in [0, 1]
           - resize_shape: tuple for resize shape (optional)
           - crop_shape: tuple for crop shape (optional)
           - rotate: boolean for rotation (optional)
@@ -28,6 +31,7 @@ class Base(metaclass=ABCMeta):
         self.dataset_dir = dataset_dir
         self.train_or_test = train_or_test
         self.batch_size = batch_size
+        self.validation_split = validation_split
 
         self.resize_shape = resize_shape
         self.crop_shape = crop_shape
@@ -38,7 +42,7 @@ class Base(metaclass=ABCMeta):
         print('Building a dataset pipeline ...')
         self._get_filenames()
         self._get_classes()
-        print('Found {} images.'.format(len(self.filenames)))
+        print('Found {} images.'.format(len(self)))
         self._build()
         print('Done.')
 
@@ -57,21 +61,9 @@ class Base(metaclass=ABCMeta):
     def num_classes(self):
         return len(self.classes)
 
-    def _build(self):
-        dataset = tf.data.Dataset.from_tensor_slices(self.samples)
-        dataset = dataset.shuffle(len(self))\
-          .map(self.read)\
-          .map(self.preprocess)\
-          .prefetch(self.batch_size)\
-          .batch(self.batch_size, drop_remainder=True)
-        if tf.__version__ >= '2.0.0':
-            self.loader = dataset
-        else:
-            self.iterator = dataset.make_one_shot_iterator()
-
     def read(self, imagefile, label):
         if tf.__version__ >= '1.14.0':
-            image = tf.io.decode(tf.io.read_file(imagefile))
+            image = tf.io.decode_image(tf.io.read_file(imagefile))
         else:
             image = tf.image.decode_image(tf.io.read_file(imagefile))
         return image, label
@@ -79,8 +71,8 @@ class Base(metaclass=ABCMeta):
     def preprocess(self, image, label):
         if self.resize_shape is not None:
             th, tw = self.resize_shape
-            image = tf.image.resize_image_with_pad(image, tf, tw)
-            image.set_shape(th, tw, 3)
+            image = tf.image.resize_image_with_pad(image, th, tw)
+            image.set_shape((th, tw, 3))
 
         if self.crop_shape is not None:
             image = tf.image.random_crop(image, (*self.crop_shape, 3))
@@ -99,6 +91,38 @@ class Base(metaclass=ABCMeta):
         image /= 255.0
         return image, label
 
+    def preprocess_valid(self, image, label):
+        if self.resize_shape is not None:
+            th, tw = self.resize_shape
+            image = tf.image.resize_image_with_pad(image, th, tw)
+            image.set_shape((th, tw, 3))
+        return image, label
+
+    def _build(self):
+        if self.train_or_test == 'train':
+            idx = int(len(self) * (1 - self.validation_split))
+            dataset = tf.data.Dataset.from_tensor_slices(self.samples)
+            dataset = dataset.shuffle(len(self))
+            train_set = dataset.take(idx)\
+              .map(self.read)\
+              .map(self.preprocess)\
+              .prefetch(self.batch_size)\
+              .batch(self.batch_size, drop_remainder=True)
+            valid_set = dataset.skip(idx)\
+              .map(self.read)\
+              .map(self.preprocess_valid)\
+              .prefetch(self.batch_size)\
+              .batch(self.batch_size, drop_remainder=True)
+            
+            if tf.__version__ >= '2.0.0':
+                self.train_loader = trainset
+                self.valid_loader = validset
+            else:
+                self.train_iterator = train_set.make_one_shot_iterator()
+                self.valid_iterator = valid_set.make_one_shot_iterator()
+        else:
+            raise NotImplementedError('Test mode is not implemented')
+        
 
 class DogsVsCats(Base):
     """ tf.data pipeline for Flower dataset
@@ -108,6 +132,7 @@ class DogsVsCats(Base):
                  dataset_dir,
                  train_or_test,
                  batch_size=1,
+                 validation_split=0.1,
                  resize_shape=None,
                  crop_shape=None,
                  rotate=False,
@@ -118,6 +143,7 @@ class DogsVsCats(Base):
           - dataset_dir: string of /path/to/dataset-directory
           - train_or_test: train or test argument
           - batch_size: int for batch size
+          - validation_split: validation split ratio, should be in [0, 1]
           - resize_shape: tuple for resize shape (optional)
           - crop_shape: tuple for crop shape (optional)
           - rotate: boolean for rotation (optional)
@@ -127,6 +153,7 @@ class DogsVsCats(Base):
         super().__init__(dataset_dir=dataset_dir,
                          train_or_test=train_or_test,
                          batch_size=batch_size,
+                         validation_split=validation_split,
                          resize_shape=resize_shape,
                          crop_shape=crop_shape,
                          rotate=rotate,
@@ -135,6 +162,14 @@ class DogsVsCats(Base):
 
     def _get_filenames(self):
         d = self.dataset_dir + '/' + self.train_or_test
+        filepath_dog = d + '/dog*.jpg'
+        filepath_cat = d + '/cat*.jpg'
+        imagefiles_dog = glob(filepath_dog)
+        imagefiles_cat = glob(filepath_cat)
+        imagefiles =  imagefiles_dog + imagefiles_cat
+        labels =  [0]*len(imagefiles_dog) + [1]*len(imagefiles_cat)
+        self.samples = (imagefiles, labels)
 
-    @abstractmethod
-    def _get_classes(self): ...
+    def _get_classes(self):
+        self.classes = ['dog', 'cat']
+        

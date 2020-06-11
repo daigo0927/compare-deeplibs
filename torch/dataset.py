824d90cc4245a49b06656db18e54608ba2214a80
Daigo Hirooka <daigo.hirooka@gmail.com>
2020-06-11 23:53:47 +0900

    update pytorch scripts #7

--- torch/dataset.py
+++ torch/dataset.py
@@ -1,119 +1,51 @@
-import numpy as np
+import cv2
 import torch
-from torch.utils.data import Dataset
-from torchvision import transforms
-from PIL import Image
+import numpy as np
+
 from glob import glob
-from abc import abstractmethod, ABCMeta
+from torch.utils.data import Dataset
 
 
-class Base(Dataset, metaclass=ABCMeta):
-    """
-    Abstract class to flexibly implement dataset pipeline
-    """
+def prepare_samples(dataset_dir, train_or_test, num_classes=10):
+    image_paths, labels = [], []
+    
+    sub_dir = dataset_dir + '/' + train_or_test
+    for c in range(num_classes):
+        query = '/'.join([sub_dir, str(c), '*.png'])
+        paths = glob(query)
+        image_paths += paths
+        labels += [c]*len(paths)
+
+    return image_paths, labels
+
+
+class Cifar10(Dataset):
     def __init__(self,
-                 dataset_dir,
-                 train_or_test,
+                 input_image_paths,
+                 target_labels,
                  preprocess=None,
-                 transform=None):
-        """
-        Args:
-          - dataset_dir: string of /path/to/dataset-directory
-          - train_or_test: train or test argument
-          - preprocess: preprocess applied to ALL data
-          - transform: data augmentation applied to TRAINING data
-         """
-        self.dataset_dir = dataset_dir
-        self.train_or_test = train_or_test
-        self.training = True
-
+                 augmentation=None):
+        self.input_image_paths = input_image_paths
+        self.target_labels = target_labels
         self.preprocess = preprocess
-        self.transform = transform
-
-        print('Building a dataset pipeline ...')
-        self._set_classes()
-        self._get_filenames()
-        print('Found {} images.'.format(len(self)))
-        print('Done.')
+        self.augmentation = augmentation
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.input_image_paths)
 
     def __getitem__(self, idx):
-        imagefile, label = self.samples[idx]
-        image, label = self._read(imagefile, label)
-        label = torch.as_tensor(label, dtype=torch.long)
+        image = cv2.imread(self.input_image_paths[idx])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         if self.preprocess:
-            image = self.preprocess(image)
-        if self.training and self.transform:
-            image = self.transform(image)
+            image = self.preprocess(image=image)['image']
+                
+        if self.augmentation:
+            image = self.augmentation(image=image)['image']
+
+        image = np.transpose(image, (2, 0, 1))
+        label = self.target_labels[idx]
+
+        image = torch.from_numpy(image)
+        label = torch.as_tensor(label, dtype=torch.long)
         return image, label
-
-    def _read(self, imagefile, label):
-        image = Image.open(imagefile)
-        label = int(label)
-        return image, label
-
-    @abstractmethod
-    def _set_classes(self):
-        """ implement self.classes """; ...
-
-    @abstractmethod
-    def _get_filenames(self):
-        """ implement self.samples """; ...
-
-    @property
-    def num_classes(self):
-        return len(self.classes)
-
-    def eval(self):
-        """ Set to evaluation mode (skip augumentation) """
-        self.training = False
-
-    def train(self):
-        """ Set to training (default) mode (executes augumentation) """
-        self.training = True
-
-
-class Cifar10(Base):
-    """ torch dataset pipeline for cifar-10 dataset
-    https://www.cs.toronto.edu/~kriz/cifar.html
-    """
-    def __init__(self,
-                 dataset_dir,
-                 train_or_test,
-                 preprocess=None,
-                 transform=None):
-        """
-        Args:
-          - dataset_dir: string of /path/to/dataset-directory
-          - train_or_test: train or test argument
-          - preprocess: preprocess applied to ALL data
-          - transform: data augmentation applied to TRAINING data
-        """
-        super().__init__(dataset_dir=dataset_dir,
-                         train_or_test=train_or_test,
-                         preprocess=preprocess,
-                         transform=transform)
-
-    def _set_classes(self):
-        self.classes = ['airplane',
-			'automobile',
-			'bird',
-			'cat',
-			'deer',
-			'dog',
-			'frog',
-			'horse',
-			'ship',
-			'truck']
-
-    def _get_filenames(self):
-        subd = self.dataset_dir + '/' + self.train_or_test
-        imagefiles, labels = [], []
-        for c in range(self.num_classes):
-            filepath = '/'.join([subd, str(c), '*.png'])
-            ifiles = glob(filepath)
-            imagefiles += ifiles
-            labels += [c]*len(ifiles)
-        self.samples = [(file, label) for file, label in zip(imagefiles, labels)]
--- torch/train.py
+++ torch/train.py
@@ -1,16 +1,12 @@
-import os, sys
-sys.path.append(os.pardir)
-import time
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms
+import argparse
+import albumentations as A
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 
 from model import ResNetMini
-from dataset import Cifar10
-from utils import prepare_parser, show_progress
+from dataset import prepare_samples, Cifar10
 
 
 def accuracy(logits, labels):
@@ -18,104 +14,43 @@ def accuracy(logits, labels):
     return (preds == labels).sum().item() / labels.size(0)
 
 
-def train(args):
-    # Create preprocesses
-    preps = []
-    if args.resize_shape:
-        preps.append(transforms.Resize(args.resize_shape))
-    preps += [
-        lambda x: np.asarray(x),
-        lambda x: x/255.0,
-        transforms.ToTensor()
-    ]
-    preprocess = transforms.Compose(preps)
-    
-    # Create augumentations
-    trans = []
-    if args.crop_shape:
-        trans.append(transforms.RandomCrop(args.crop_shape))
-    if args.rotate:
-        trans.append(transforms.RandomRotate(45))
-    if args.flip_left_right:
-        trans.append(transforms.RandomFlipLeftRight())
-    if args.flip_up_down:
-        trans.append(transforms.RandomFlipUpDown())
-    transform = transforms.Compose(trans)
+def train(dataset_dir, batch_size, learning_rate, epochs, num_workers):
 
-    # ------------- Build dataset pipeline -----------------
-    dataset = Cifar10(dataset_dir=args.dataset_dir,
-                      train_or_test='train',
-                      preprocess=preprocess,
-                      transform=transform)
+    input_paths, target_labels = prepare_samples(dataset_dir, train_or_test='train')
+    preprocess = A.Compose([A.Normalize()])
+    augmentation = None
+    dataset = Cifar10(input_paths, target_labels,
+                      preprocess=preprocess, augmentation=augmentation)
+    dataloader = DataLoader(dataset, shuffle=True,
+                            batch_size=batch_size, num_workers=num_workers)
 
-    # Build dataloader
-    n_train = int(len(dataset)*(1-args.validation_split))
-    n_val = len(dataset) - n_train
-    tset, vset = random_split(dataset, [n_train, n_val])
-    tloader = DataLoader(tset, args.batch_size, shuffle=True, num_workers=1)
-    vloader = DataLoader(vset, args.batch_size, shuffle=False, num_workers=1)
-
-    # ------------- Build model and optimizer-----------------
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = ResNetMini(args.filters,
-                       dataset.num_classes).to(device)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = ResNetMini(10).to(device)
     criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # ------------- Training loop -----------------
-    start_loop = time.time()
-    for e in range(args.epochs):
-        start_epoch = time.time()
-        
-        model.train()
-        tset.dataset.train()
-        for i, (images, labels) in enumerate(tloader):
-            start_batch = time.time()
-            images, labels = images.to(device, torch.float32), labels.to(device)
-            model.zero_grad()
+    for e in range(epochs):
+        running_acc = 0.0
+        for i, (images, labels) in enumerate(tqdm(dataloader)):
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
             logits = model(images)
             loss = criterion(logits, labels)
             loss.backward()
             optimizer.step()
-            batch_time = time.time() - start_batch
 
-            if i%args.check_step == 0 or i+1 == len(tloader): # ----- Output log -----
-                acc = accuracy(logits, labels)
-                show_progress(e+1, i+1, len(tloader),
-                              loss=loss.item(), accuracy=acc,
-                              batch_time=batch_time)
-
-        train_time = time.time() - start_epoch
-        print('\nTraining time: {}.'.format(train_time))
-
-        # ------------- Evaluation -----------------
-        model.eval()
-        vset.dataset.eval()
-        losses, accs = [], []
-        for images, labels in vloader:
-            images, labels = images.to(device, torch.float32), labels.to(device)
-            logits = model(images)
-            loss = criterion(logits, labels)
-            acc = accuracy(logits, labels)
-            losses.append(loss.item())
-            accs.append(acc)
-
-        val_time = time.time() - start_epoch - train_time
-        print('Validation score: loss: {}, accuracy: {}, time: {}.'\
-              .format(np.mean(losses), np.mean(accs), val_time))
-
-        epoch_time = time.time() - start_epoch
-        print('Epoch time: {}sec'.format(epoch_time))
-        print()
-
-    loop_time = time.time() - start_loop
-    print('Total time: {}sec.'.format(loop_time))
+            running_acc += accuracy(logits, labels)
+        print(f'Epoch: {epoch}, accuracy: {running_acc/(i+1)}')
 
 
 if __name__ == '__main__':
-    parser = prepare_parser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset-dir', default='../cifar-10')
+    parser.add_argument('-b', '--batch-size', default=32)
+    parser.add_argument('-lr', '--learning-rate', default=0.001)
+    parser.add_argument('-e', '--epochs', default=10)
+    parser.add_argument('-nw', '--num-workers', default=0)
     args = parser.parse_args()
-    for k, v in vars(args).items():
-        print('{}: {}'.format(k, v))
+    
+    train(**vars(args))
 
-    train(args)
